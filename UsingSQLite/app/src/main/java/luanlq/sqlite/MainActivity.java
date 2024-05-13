62b0cfaa14b91ba292020dbc9af4329e0a000533
Lequocluan <quocluan15102003@gmail.com>
2024-05-14 00:59:27 +0700

    Using SQLite

---
+++ UsingSQLite/app/src/main/java/luanlq/sqlite/MainActivity.java
@@ -0,0 +1,48 @@
+package luanlq.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycle);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        SQLiteDatabase database;
+        database = openOrCreateDatabase("QLSV.db", MODE_PRIVATE, null);
+
+        String taoBang = "CREATE TABLE SinhVien (id integer PRIMARY KEY, name text, lop text, hocLuc text)";
+        String xoaBang = "DROP TABLE IF EXISTS SinhVien";
+        database.execSQL(xoaBang);
+        database.execSQL(taoBang);
+
+        String sqlThem1 = "INSERT INTO SinhVien VALUES (63132681, 'Lê Văn Tiến', '63.CNTT-3', 'Giỏi')";
+        String sqlThem2 = "INSERT INTO SinhVien VALUES (1123214, 'Lê Văn A', '63.CNTT-2', 'Khá')";
+        String sqlThem3 = "INSERT INTO SinhVien VALUES (4252, 'Lê Văn B', '63.CNTT-4', 'Trung bình')";
+        String sqlThem4 = "INSERT INTO SinhVien VALUES (1543, 'Lê Văn C', '63.CNTT-5', 'Giỏi')";
+        String sqlThem5 = "INSERT INTO SinhVien VALUES (41235, 'Lê Văn E', '63.CNTT-2', 'Giỏi')";
+        String sqlThem6 = "INSERT INTO SinhVien VALUES (1512, 'Lê Văn F', '63.CNTT-3', 'Giỏi')";
+        String sqlThem7 = "INSERT INTO SinhVien VALUES (5735, 'Lê Văn G', '63.CNTT-4', 'Giỏi')";
+        String sqlThem8 = "INSERT INTO SinhVien VALUES (9572, 'Lê Văn H', '63.CNTT-1', 'Giỏi')";
+        database.execSQL(sqlThem1);
+        database.execSQL(sqlThem2);
+        database.execSQL(sqlThem3);
+        database.execSQL(sqlThem4);
+        database.execSQL(sqlThem5);
+        database.execSQL(sqlThem6);
+        database.execSQL(sqlThem7);
+        database.execSQL(sqlThem8);
+        database.close();
+    }
+}
